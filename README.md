1db6b377b68578c66690277ea01a314b9fac4a6d
outdead <outdead@mail.ru>
2019-07-27 03:26:08 +0300

    cli untested

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # rcon-cli
-Rcon cli
+[![Go Report Card](https://goreportcard.com/badge/github.com/gorcon/rcon-cli)](https://goreportcard.com/report/github.com/gorcon/rcon-cli)
+
+CLI for executing queries on a remote server
+
+## Supported Games
+
+* [Project Zomboid](https://store.steampowered.com/app/108600) 
+* [Conan Exiles](https://store.steampowered.com/app/440900)
+* [Rust](https://store.steampowered.com/app/252490) (add +rcon.web 0 to the args when starting the server)
+* [ARK: Survival Evolved](https://store.steampowered.com/app/346110)
+
+Open pull request if you have successfully used a package with another game with rcon support and add it to the list.
+
+## Installation
+
+Download the binary for your platform from the [releases](https://github.com/gorcon/rcon/releases)
+
+## Usage
+
+Rcon CLI can be run in two modes - in the mode of a single query and in the mode of reading the input stream
+
+### Single mode
+
+Server address, password and command to server must be specified in flags at startup. Example:
+
+    ./rcon -a address -p password -c command
+    
+If flags are passed into the CLI, then the flags are sent as a single command. 
+The response is displayed, and the CLI will exit.
+
+### Input stream mode
+
+To run CLI in input stream mode add command cli. Example:
+
+    ./rcon -a address -p password cli
+    
+Use ^C to terminate or type command :q in CLI.    
+
+## Configuration file
+
+For more convenient use, the ability to create the rcon.yaml configuration file is provided. 
+You can save the address of the remote server and its password. If the configuration file exists, 
+then at startup the -a and -p flags can be omitted. 
+
+File must be saved in yaml format. Example:
+
+```yaml
+default:
+  address: "127.0.0.1:16260"
+  password: "password"
+```
---
+++ main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/go-yaml/yaml"
+	"github.com/gorcon/rcon"
+	"github.com/urfave/cli"
+)
+
+// DefaultConfigName sets the default config file name.
+const DefaultConfigName = "rcon.yaml"
+
+// Config allows to take a remote server address and password from
+// the configuration file. This enables not to specify these flags when
+// running the CLI.
+//
+// Example:
+// ```yaml
+// default:
+//   address: "127.0.0.1:16260"
+//   password: "password"
+// ```
+type Config map[string]struct {
+	Address  string `json:"address" yaml:"address"`
+	Password string `json:"password" yaml:"password"`
+}
+
+func main() {
+	var description = "Can be run in two modes - in the mode of a single query"
+	description += "\n   and in the mode of reading the input stream"
+
+	app := cli.NewApp()
+	app.Usage = "CLI for executing queries on a remote server"
+	app.Description = description
+	app.Version = "0.1.0"
+	app.Author = "Pavel Korotkiy (outdead)"
+	app.Copyright = "Copyright (c) 2019 Pavel Korotkiy"
+	app.Commands = []cli.Command{
+		{
+			Name:  "cli",
+			Usage: "Run CLI for commands in the form of successive lines of text from the input stream",
+			Action: func(c *cli.Context) error {
+				address, password := getCredentials(c)
+				if address == "" || password == "" {
+					cli.ShowAppHelpAndExit(c, 1)
+					return nil
+				}
+
+				scanner := bufio.NewScanner(os.Stdin)
+				fmt.Print("> ")
+				for scanner.Scan() {
+					command := scanner.Text()
+					if command != "" {
+						if command == ":q" {
+							return nil
+						}
+
+						execute(address, password, command)
+					}
+
+					fmt.Print("> ")
+				}
+
+				return nil
+			},
+		},
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name: "address, a",
+			Usage: "set host and port to remote rcon server. Example 127.0.0.1:16260" +
+				"\n                              can be set in the config file rcon.yaml",
+		},
+		cli.StringFlag{
+			Name: "password, p",
+			Usage: "set password to remote rcon server" +
+				"\n                               can be set in the config file rcon.yaml",
+		},
+		cli.StringFlag{
+			Name:  "command, c",
+			Usage: "command to execute on remote server. Required flag to run in single mode",
+		},
+		cli.StringFlag{
+			Name:  "env, e",
+			Usage: "allows you to select remote server address and password from the environment in the configuration file",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		address, password := getCredentials(c)
+		command := c.String("command")
+		if address == "" || password == "" || command == "" {
+			cli.ShowAppHelpAndExit(c, 1)
+			return nil
+		}
+
+		execute(address, password, command)
+		return nil
+	}
+
+	app.Run(os.Args)
+}
+
+// execute sends command to execute to the remote server.
+func execute(address string, password string, command string) {
+	console, err := rcon.Dial(address, password)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer console.Close()
+
+	result, err := console.Execute(command)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if result != "" {
+		fmt.Println(result)
+	}
+}
+
+// readYamlConfig reads config data from yaml file.
+func readYamlConfig(path string) (Config, error) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err = yaml.Unmarshal(file, &cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+// getCredentials parses os args or config file for details of connecting to
+// a remote server. If the address and password flags were received, the
+// configuration file is ignored.
+func getCredentials(c *cli.Context) (address string, password string) {
+	address = c.String("a")
+	password = c.String("p")
+
+	if address != "" && password != "" {
+		return
+	}
+
+	home, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	path := home + "/" + DefaultConfigName
+
+	if _, err := os.Stat(path); err == nil {
+		cfg, err := readYamlConfig(path)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if address == "" {
+			address = cfg["default"].Address
+		}
+
+		if password == "" {
+			password = cfg["default"].Password
+		}
+	}
+
+	return
+}
